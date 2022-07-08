bc88cf8b7248161070bc1841fb2c88dccc335688
aliakgoz <aliakgoz@gmail.com>
2022-07-08 17:32:44 +0300

    temizlik

--- js/main.js
+++
@@ -1,5 +0,0 @@
-
-import { initializeApp } from './firebase-app.js';
-import { getDatabase, ref, set, child, update, remove, onValue } from  './firebase-database.js';
-
-export { initializeApp, getDatabase, ref, set, child, update, remove, onValue, DataSnapshot };
